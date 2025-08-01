aa80f0b28ec9aae1cc4d123bc5b0d8934fe0f61e
Sky-Blue <akash16260102@gmail.com>
2025-08-02 05:07:03 +0530

    link added

--- index/index.js
+++ index/index.js
@@ -1,5 +1,5 @@
 
 setTimeout(function() {
-        window.location.href = "/home/home.html"; 
+        window.location.href = "https://techy-ui.github.io/Project-Pig-2.0/home/home.html"; 
     }, 7000); 
 
